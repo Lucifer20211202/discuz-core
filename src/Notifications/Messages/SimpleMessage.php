29230507557047d11ec260588fe4a94f986ac7ab
cjwjkcqw <13627305664@163.com>
2021-03-04 14:36:40 +0800

    bugfix:处理问题单258-内容审核通知的表情不解析

--- src/Notifications/Messages/SimpleMessage.php
+++ src/Notifications/Messages/SimpleMessage.php
@@ -20,9 +20,6 @@ abstract class SimpleMessage
     protected function getContent($data)
     {
         $replaceVars = array_map(function ($var) {
-            if (is_string($var) && $this->filterSpecialChar) {
-                $var = htmlspecialchars($var);
-            }
             return $var;
         }, $this->contentReplaceVars($data));
 
